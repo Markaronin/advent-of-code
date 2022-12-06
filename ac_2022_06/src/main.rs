6c201f63aa7d1bf098003fc86ba973d246d252b1
Markaronin <jmarkroylance@gmail.com>
2022-12-06 08:51:39 -0700

    Finish ac_2022_06

---
+++ ac_2022_06/src/main.rs
@@ -0,0 +1,22 @@
+use advent_of_code_util::*;
+use itertools::Itertools;
+
+fn find_packet_start(input: &Vec<char>, num_unique: usize) -> usize {
+    input
+        .windows(num_unique)
+        .find_position(|window| window.into_iter().unique().count() == window.len())
+        .unwrap()
+        .0
+        + num_unique
+}
+
+fn get_program_output(input_file: &str) -> (usize, usize) {
+    let input = read_lines(input_file).get(0).unwrap().chars().collect_vec();
+
+    let result_1 = find_packet_start(&input, 4);
+    let result_2 = find_packet_start(&input, 14);
+
+    (result_1, result_2)
+}
+
+base_aoc!(7, 19);
