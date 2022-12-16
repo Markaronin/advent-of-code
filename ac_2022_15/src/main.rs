a2316be75c94cd1e1497f4c710b25fbb5b74ad7a
Markaronin <jmarkroylance@gmail.com>
2022-12-15 19:52:32 -0700

    Finished ac_2022_15

--- ac_2022_15/src/main.rs
+++ ac_2022_15/src/main.rs
@@ -46,9 +46,47 @@ impl Sensor {
 
         (start_x..=end_x).map(|x| Coordinate { x, y }).collect_vec()
     }
+
+    pub fn square_outside_boundary(&self) -> Vec<Coordinate> {
+        let distance_past_beacon = self.position.manhatten_distance(&self.beacon) + 1;
+
+        (0..distance_past_beacon)
+            .flat_map(|i| {
+                [
+                    Coordinate {
+                        x: self.position.x + i,
+                        y: self.position.y + (distance_past_beacon - i),
+                    },
+                    Coordinate {
+                        x: self.position.x + (distance_past_beacon - i),
+                        y: self.position.y - i,
+                    },
+                    Coordinate {
+                        x: self.position.x - i,
+                        y: self.position.y - (distance_past_beacon - i),
+                    },
+                    Coordinate {
+                        x: self.position.x - (distance_past_beacon - i),
+                        y: self.position.y + i,
+                    },
+                ]
+            })
+            .collect_vec()
+    }
+
+    pub fn is_inside_boundary(&self, coord: &Coordinate) -> bool {
+        let distance_to_beacon = self.position.manhatten_distance(&self.beacon);
+        let distance_to_point = self.position.manhatten_distance(coord);
+
+        distance_to_beacon >= distance_to_point
+    }
 }
 
-fn get_program_output(input_file: &str, part_1_y: isize) -> (usize, usize) {
+fn get_program_output(
+    input_file: &str,
+    part_1_y: isize,
+    part_2_bounds: Coordinate,
+) -> (usize, usize) {
     let input = read_lines(input_file)
         .into_iter()
         .map(|line| Sensor::from_line(&line))
@@ -62,8 +100,23 @@ fn get_program_output(input_file: &str, part_1_y: isize) -> (usize, usize) {
         .count();
 
     let result_2 = {
-        let only_position = Coordinate { x: 0, y: 0 };
-        todo!();
+        let all_possible_points = input
+            .iter()
+            .flat_map(|sensor| sensor.square_outside_boundary())
+            .unique()
+            .filter(|point| {
+                point.x >= 0
+                    && point.y >= 0
+                    && point.x <= part_2_bounds.x
+                    && point.y <= part_2_bounds.y
+            })
+            .filter(|point| !input.iter().any(|sensor| sensor.is_inside_boundary(point)))
+            .collect_vec();
+
+        assert_eq!(all_possible_points.len(), 1);
+
+        let only_position = &all_possible_points[0];
+
         ((only_position.x * 4000000) + only_position.y) as usize
     };
 
@@ -77,7 +130,8 @@ mod tests {
     #[test]
     fn main() {
         let file_path = format!("{}/testinput", env!("CARGO_MANIFEST_DIR"));
-        let (part_1_output, part_2_output) = get_program_output(&file_path, 10);
+        let (part_1_output, part_2_output) =
+            get_program_output(&file_path, 10, crate::Coordinate { x: 20, y: 20 });
         assert_eq!(part_1_output, 26);
         assert_eq!(part_2_output, 56000011);
     }
@@ -85,7 +139,14 @@ mod tests {
 
 fn main() {
     let file_path = format!("{}/input", env!("CARGO_MANIFEST_DIR"));
-    let (part_1_output, part_2_output) = get_program_output(&file_path, 2000000);
+    let (part_1_output, part_2_output) = get_program_output(
+        &file_path,
+        2000000,
+        Coordinate {
+            x: 4000000,
+            y: 4000000,
+        },
+    );
     println!("Part 1 output: {}", part_1_output);
     println!("Part 2 output: {}", part_2_output);
 }
