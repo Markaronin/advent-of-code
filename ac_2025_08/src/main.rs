02fb861b63aa5baaa26d4c2f9d1aa8f7b7e91b67
Markaronin <jmarkroylance@gmail.com>
2025-12-08 09:03:50 -0700

    Use union find

--- ac_2025_08/src/main.rs
+++ ac_2025_08/src/main.rs
@@ -1,10 +1,38 @@
-use std::{
-    cmp::Reverse,
-    collections::{BTreeMap, BTreeSet, BinaryHeap},
-};
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 use advent_of_code_util::{base_aoc, parse::read_lines};
 use itertools::Itertools;
+
+struct UnionFind {
+    parents: Vec<usize>,
+    size: Vec<usize>,
+}
+impl UnionFind {
+    pub fn size(&self, i: usize) -> usize {
+        self.size[self.find(i)]
+    }
+
+    pub fn union(&mut self, i: usize, j: usize) {
+        let i = self.find(i);
+        let j = self.find(j);
+        if i != j {
+            if self.size(i) < self.size(j) {
+                self.parents[i] = j;
+                self.size[j] += self.size[i];
+            } else {
+                self.parents[j] = i;
+                self.size[i] += self.size[j];
+            }
+        }
+    }
+
+    pub fn find(&self, mut i: usize) -> usize {
+        while self.parents[i] != i {
+            i = self.parents[i];
+        }
+        i
+    }
+}
 
 fn get_program_output(coords_file: &str) -> (usize, usize) {
     let coords = read_lines(coords_file)
@@ -29,61 +57,26 @@ fn get_program_output(coords_file: &str) -> (usize, usize) {
     }
 
     // Loop through connections, updating both sets to actually be the same set
-    let mut idx_to_circuit: Vec<Option<usize>> = vec![None; coords.len()];
-    let mut circuits: BTreeMap<usize, BTreeSet<usize>> = BTreeMap::new();
-    let mut new_circuit_index = 0;
+    let mut uf = UnionFind {
+        parents: (0..coords.len()).collect_vec(),
+        size: vec![1; coords.len()],
+    };
     let num_connections = if cfg!(test) { 10 } else { 1000 };
     let mut current_connection = 1;
     let mut part_1 = None;
     let mut part_2 = None;
 
-    while circuits.len() > 1
-        || new_circuit_index < 2
-        || circuits.first_entry().unwrap().get().len() < coords.len()
-    {
+    while uf.size(0) < coords.len() {
         let (_, first, second) = connections.pop().unwrap().0;
 
-        match (idx_to_circuit[first], idx_to_circuit[second]) {
-            (None, None) => {
-                let circuit_index = new_circuit_index;
-                new_circuit_index += 1;
-                let mut circuit = BTreeSet::new();
-                circuit.insert(first);
-                circuit.insert(second);
-                circuits.insert(circuit_index, circuit);
-                idx_to_circuit[first] = Some(circuit_index);
-                idx_to_circuit[second] = Some(circuit_index);
-            }
-            (None, Some(second_circuit)) => {
-                idx_to_circuit[first] = Some(second_circuit);
-                circuits.get_mut(&second_circuit).unwrap().insert(first);
-            }
-            (Some(first_circuit), None) => {
-                idx_to_circuit[second] = Some(first_circuit);
-                circuits.get_mut(&first_circuit).unwrap().insert(second);
-            }
-            (Some(first_circuit), Some(second_circuit)) if first_circuit == second_circuit => {
-                // Do nothing
-            }
-            (Some(first_circuit), Some(second_circuit)) => {
-                // Merge the circuits of first and second together
-                let second_circuit_coords = circuits.remove(&second_circuit).unwrap();
-                for second_circuit_coord in second_circuit_coords {
-                    circuits
-                        .get_mut(&first_circuit)
-                        .unwrap()
-                        .insert(second_circuit_coord);
-                    idx_to_circuit[second_circuit_coord] = Some(first_circuit);
-                }
-                circuits.remove(&second_circuit);
-            }
-        }
+        uf.union(first, second);
 
         if current_connection == num_connections {
             part_1 = Some(
-                circuits
-                    .values()
-                    .map(|v| v.len())
+                (0..coords.len())
+                    .map(|i| uf.find(i))
+                    .unique()
+                    .map(|i| uf.size(i))
                     .sorted()
                     .rev()
                     .take(3)
@@ -91,10 +84,7 @@ fn get_program_output(coords_file: &str) -> (usize, usize) {
             );
         }
 
-        if circuits.len() == 1
-            && new_circuit_index > 1
-            && circuits.first_entry().unwrap().get().len() == coords.len()
-        {
+        if uf.size(0) == coords.len() {
             part_2 = Some(coords[first].0 * coords[second].0);
         }
 
